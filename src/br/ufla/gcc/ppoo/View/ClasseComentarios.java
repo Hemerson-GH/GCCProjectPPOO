8a5c36c8ee97948b2db17dee6380cd3d84a6c070
Hemerson-GH <hemersonel@gmail.com>
2017-11-25 19:30:28 -0200

    add commit ClasseComentarios

--- src/br/ufla/gcc/ppoo/View/ClasseComentarios.java
+++ src/br/ufla/gcc/ppoo/View/ClasseComentarios.java
@@ -2,6 +2,33 @@ package br.ufla.gcc.ppoo.View;
 
 public class ClasseComentarios {
 
-	
+//	slider.addChangeListener(new ChangeListener() {
+//			public void stateChanged(ChangeEvent e) {
+//				JSlider source = (JSlider)e.getSource();
+//			    
+//			    if (!source.getValueIsAdjusting()) {
+//			        int fps = (int)slider.getValue();
+//			        if (fps == 0) {
+//			        	slider.setToolTipText("Péssimo");
+//			        	System.out.println("Péssimo");
+//			        } else if (fps == 1) {
+//			        	slider.setToolTipText("Ruim");
+//			        	System.out.println("Ruim");
+//			        } else if (fps == 2){
+//			        	slider.setToolTipText("Regular");
+//			        	System.out.println("Regular");
+//			        } else if (fps == 3){
+//			        	slider.setToolTipText("Bom");
+//			        	System.out.println("Bom");
+//			        } else if (fps == 4) {
+//			        	slider.setToolTipText("Muito Bom");
+//			        	System.out.println("Muito Bom");
+//			        } else if (fps == 5) {
+//			        	slider.setToolTipText("Excelente");
+//			        	System.out.println("Excelente");
+//			        }
+//			    }
+//			}
+//		});
 	
 }
--- src/br/ufla/gcc/ppoo/View/TelaPrincipal.java
+++ src/br/ufla/gcc/ppoo/View/TelaPrincipal.java
@@ -67,34 +67,7 @@ public class TelaPrincipal {
 		slider.setBounds(667, 166, 90, 130);
 		slider.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
 		slider.setBackground(Color.WHITE);
-//		slider.addChangeListener(new ChangeListener() {
-//			public void stateChanged(ChangeEvent e) {
-//				JSlider source = (JSlider)e.getSource();
-//			    
-//			    if (!source.getValueIsAdjusting()) {
-//			        int fps = (int)slider.getValue();
-//			        if (fps == 0) {
-//			        	slider.setToolTipText("Péssimo");
-//			        	System.out.println("Péssimo");
-//			        } else if (fps == 1) {
-//			        	slider.setToolTipText("Ruim");
-//			        	System.out.println("Ruim");
-//			        } else if (fps == 2){
-//			        	slider.setToolTipText("Regular");
-//			        	System.out.println("Regular");
-//			        } else if (fps == 3){
-//			        	slider.setToolTipText("Bom");
-//			        	System.out.println("Bom");
-//			        } else if (fps == 4) {
-//			        	slider.setToolTipText("Muito Bom");
-//			        	System.out.println("Muito Bom");
-//			        } else if (fps == 5) {
-//			        	slider.setToolTipText("Excelente");
-//			        	System.out.println("Excelente");
-//			        }
-//			    }
-//			}
-//		});
+	
 		slider.setEnabled(true);
 		viewMain.getContentPane().add(slider);
 		slider.setMajorTickSpacing(5);
