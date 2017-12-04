db71472a74c93d29adc2c5ac79cc543c8af4a5d0
Hemerson-GH <hemersonel@gmail.com>
2017-12-04 01:57:27 -0200

    Executable create

--- src/br/ufla/gcc/ppoo/View/Principal.java
+++ src/br/ufla/gcc/ppoo/View/Principal.java
@@ -1,13 +1,8 @@
 package br.ufla.gcc.ppoo.View;
 
-import br.ufla.gcc.ppoo.Dados.DadosLogin;
-
 public class Principal {
 
 	public static void main(String[] args) {
-//		new TelaLogin();
-		DadosLogin dl = new DadosLogin("hemerson", "hemerson@gmail.com", "seagate01");
-		new TelaPrincipal(dl);
+		new TelaLogin();
 	}
-
 }
